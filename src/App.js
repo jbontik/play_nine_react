85a8f896860b7b631addc9af0e478a8c9d5a2afe
Jean <jean@bon.com>
2017-01-05 09:49:00 +0100

    Add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,21 +1,61 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import "./App.css";
+
+class StarsFrame extends Component {
+    render() {
+        return (
+            <div id="stars-frame">
+                <div className="well">
+                    <span className="glyphicon glyphicon-star"/>
+                    <span className="glyphicon glyphicon-star"/>
+                    <span className="glyphicon glyphicon-star"/>
+                    <span className="glyphicon glyphicon-star"/>
+                </div>
+            </div>
+        );
+    }
+}
+
+class ButtonFrame extends Component {
+    render() {
+        return (
+            <div id="button-frame">
+                <button className="btn btn-primary">=</button>
+            </div>
+        );
+    }
+}
+
+
+class AnswerFrame extends Component {
+    render() {
+        return (
+            <div id="answer-frame">
+                <div className="well">...</div>
+            </div>
+        );
+    }
+}
+
+class Game extends Component {
+    render() {
+        return (
+            <div id="game">
+                <h2>Play Nine</h2>
+                <StarsFrame/>
+                <ButtonFrame/>
+                <AnswerFrame/>
+            </div>
+        );
+    }
+}
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Game/>
+        );
+    }
 }
 
 export default App;
